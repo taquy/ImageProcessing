40dfe5f5a718f61adc033bfd150851f16e209f4b
hoadx <xuanhoa.master@gmail.com>
2018-01-20 16:16:30 +0700

    bg subtraction mog

---
+++ Z_BGSubtraction/__init__.py
@@ -0,0 +1,13 @@
+import cv2 as cv
+
+cap = cv.VideoCapture('vtest.avi')
+fgbg = cv.bgsegm.createBackgroundSubtractorMOG()
+while (1):
+    ret, frame = cap.read()
+    fgmask = fgbg.apply(frame)
+    cv.imshow('frame', fgmask)
+    k = cv.waitKey(30) & 0xff
+    if k == 27:
+        break
+cap.release()
+cv.destroyAllWindows()
